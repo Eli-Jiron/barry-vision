360176589e9782e5077bae2fefa751760b7d547e
Eli-Jiron <elianguevara28@gmail.com>
2024-07-02 01:11:00 -0600

    feat: agregar productos

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,7 +1,69 @@
-const Admin = () => {
-  return (
-    <div>Admin</div>
-  )
-}
+import Input from "../components/ui/Input";
+import Form from "../components/Form";
+import { useRef, useState } from "react";
+import { validar } from "../utils/validaciones";
+import { postData } from "../services/fetch";
 
-export default Admin
+const Admin = () => {
+  const inputName = useRef();
+  const inputInfo = useRef();
+  const inputPrice = useRef();
+  const inputUrl = useRef();
+  const [msg, setMsg] = useState();
+  const apiUrl = "http://localhost:3000/products";
+
+  const addProduct = async (name, info, price, url) => {
+    if (validar.vacio(name, info, price, url)) {
+      setMsg("Debe completar todos lo campos");
+    } else {
+      const promise = await postData(apiUrl, {
+        name: name,
+        description: info,
+        price: price,
+        url: url,
+      });
+      if (!promise) {
+        alert("Ha ocurrido un error, intentelo más tarde");
+      }
+    }
+  };
+
+  return (
+    <main className="m-4">
+      <div>
+        <Form
+          handleClick={() =>
+            addProduct(
+              inputName.current.value,
+              inputInfo.current.value,
+              inputPrice.current.value,
+              inputUrl.current.value
+            )
+          }
+          text="Agregar"
+        >
+          <div>
+            <label htmlFor="name">Nombre:</label>
+            <Input inputRef={inputName} id="name" />
+          </div>
+          <div>
+            <label htmlFor="info">Descripción:</label>
+            <Input inputRef={inputInfo} id="info" />
+          </div>
+          <div>
+            <label htmlFor="price">Precio:</label>
+            <Input inputRef={inputPrice} id="price" />
+          </div>
+          <div>
+            <label htmlFor="url">Imagen:</label>
+            <Input inputRef={inputUrl} id="url" />
+            <p>Debe ser un Url</p>
+          </div>
+          <p>{msg}</p>
+        </Form>
+      </div>
+    </main>
+  );
+};
+
+export default Admin;
