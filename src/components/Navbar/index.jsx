4462ac2e1d759fa75992865f41c879f4064f0910
Eli-Jiron <elianguevara28@gmail.com>
2024-07-09 16:24:45 -0600

    estilos: barra de navegacion lateral

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,73 +4,73 @@ import { useNewContext } from "../../context/ContextProvider";
 const Navbar = () => {
   const { location, size } = useNewContext();
 
-  if (size.width <= 464) {
-    return null;
-  } else {
-    return (
-      <nav className="border-b-2 border-b-ice-blue h-7 drop-shadow-sm">
-        <div>
-          <ul className="flex justify-center gap-5 font-semibold">
-            <li>
-              <Link
-                className={`${
-                  location.pathname === "/" &&
-                  "text-light-green border-b-2 border-b-light-green"
-                } hover:text-light-green hover:border-b-light-green hover:border-b-2`}
-                to="/"
-              >
-                Inicio
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`${
-                  location.pathname === "/pharmacy" &&
-                  "text-light-green border-b-2 border-b-light-green"
-                } hover:text-light-green hover:border-b-light-green hover:border-b-2`}
-                to="/pharmacy"
-              >
-                Farmacia
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`${
-                  location.pathname === "/glasses" &&
-                  "text-light-green border-b-2 border-b-light-green"
-                } hover:text-light-green hover:border-b-light-green hover:border-b-2`}
-                to="/glasses"
-              >
-                Lentes
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`${
-                  location.pathname === "/about-us" &&
-                  "text-light-green border-b-2 border-b-light-green"
-                } hover:text-light-green hover:border-b-light-green hover:border-b-2`}
-                to="/about-us"
-              >
-                Sobre nosotros
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`${
-                  location.pathname === "/contact" &&
-                  "text-light-green border-b-2 border-b-light-green"
-                } hover:text-light-green hover:border-b-light-green hover:border-b-2`}
-                to="/contact"
-              >
-                Contactenos
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+  return (
+    <nav
+      className={`${
+        size.width <= 464 ? "hidden" : "block"
+      } border-b-2 border-b-ice-blue h-7 drop-shadow-sm sticky bg-white`}
+    >
+      <div>
+        <ul className="flex justify-center gap-5 font-semibold">
+          <li>
+            <Link
+              className={`${
+                location.pathname === "/" &&
+                "text-light-green border-b-2 border-b-light-green"
+              } hover:text-light-green hover:border-b-light-green hover:border-b-2`}
+              to="/"
+            >
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`${
+                location.pathname === "/pharmacy" &&
+                "text-light-green border-b-2 border-b-light-green"
+              } hover:text-light-green hover:border-b-light-green hover:border-b-2`}
+              to="/pharmacy"
+            >
+              Farmacia
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`${
+                location.pathname === "/glasses" &&
+                "text-light-green border-b-2 border-b-light-green"
+              } hover:text-light-green hover:border-b-light-green hover:border-b-2`}
+              to="/glasses"
+            >
+              Lentes
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`${
+                location.pathname === "/about-us" &&
+                "text-light-green border-b-2 border-b-light-green"
+              } hover:text-light-green hover:border-b-light-green hover:border-b-2`}
+              to="/about-us"
+            >
+              Sobre nosotros
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`${
+                location.pathname === "/contact" &&
+                "text-light-green border-b-2 border-b-light-green"
+              } hover:text-light-green hover:border-b-light-green hover:border-b-2`}
+              to="/contact"
+            >
+              Contactenos
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -3,17 +3,28 @@ const Modal = ({ children, Open, Close, title }) => {
     return null;
   } else {
     return (
-      <div className="fixed inset-0 z-10 overflow-hidden flex items-center justify-center">
-        <div className="bg-white rounded-md shadow-xl overflow-hidden max-w-md w-full sm:w-96 md:w-1/2 lg:w-2/3 xl:w-1/3 z-50">
-          <div className="bg-light-green text-white px-4 py-2 flex justify-between">
-            <h2 className="text-lg font-semibold">{title}</h2>
-          </div>
-          <div className="p-4 flex flex-col gap-3">
-            {children}
-            <button className="ring-2 ring-red rounded-sm text-red font-semibold py-1 w-full" onClick={Close}>Cerrar</button>
+      <>
+        <div className="fixed inset-0 z-10 overflow-hidden bg-black opacity-60"></div>
+        <div
+          onClick={Close}
+          className="fixed inset-0 z-20 overflow-hidden flex items-center justify-center"
+        >
+          <div className="bg-white rounded-md shadow-xl overflow-hidden max-w-md w-full sm:w-96 md:w-1/2 lg:w-2/3 xl:w-1/3 z-50">
+            <div className="bg-light-green text-white px-4 py-2 flex justify-between">
+              <h2 className="text-lg font-semibold">{title}</h2>
+            </div>
+            <div className="p-4 flex flex-col gap-3">
+              {children}
+              <button
+                className="ring-2 ring-red rounded-sm text-red font-semibold py-1 w-full"
+                onClick={Close}
+              >
+                Cerrar
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 };
