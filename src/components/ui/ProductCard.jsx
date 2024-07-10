624b07173490ae50c2ce8e87c434d11261ffba72
Eli-Jiron <elianguevara28@gmail.com>
2024-07-10 10:36:21 -0600

    estilos: descuento en las tarjetas de productos

--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -13,16 +13,50 @@ const ProductCard = (props) => {
               src={props.url}
               alt="product"
             />
-            <p className="my-4 pl-4 font-bold text-light-green">{props.name}</p>
-            <p className="mb-4 ml-4 text-xl font-semibold text-blue-gray-700">
-              {`₡${props.price}`}
-            </p>
-            {location.pathname === "/admin" && (
-              <div className="flex justify-center gap-3">
-                <button onClick={props.editClick}>Editar</button>
-                <button onClick={props.deleteClick}>Eliminar</button>
+            <div className="py-2 px-2">
+              <p className=" font-bold text-light-green">{props.name}</p>
+              <div className="flex justify-between gap-3">
+                <div>
+                  {props.discount > 0 && (
+                    <p className="text-base font-semibold text-blue-gray-700">
+                      Antes:
+                    </p>
+                  )}
+                  <p
+                    className={`${
+                      props.discount > 0 && "line-through"
+                    } text-lg font-semibold text-blue-gray-700`}
+                  >
+                    {`₡${props.price}`}
+                  </p>
+                </div>
+                {props.discount > 0 && (
+                  <>
+                    <div>
+                      <p className="text-base font-semibold text-blue-gray-700">
+                        Ahora:
+                      </p>
+                      <p className="text-lg font-semibold text-blue-gray-700">
+                        {`₡${Math.floor(
+                          props.price - props.price * (props.discount / 100)
+                        )}`}
+                      </p>
+                    </div>
+                    <div className="flex items-center">
+                      <p className="flex justify-center items-center h-9 w-9 bg-red rounded-full  ml-auto text-sm font-semibold text-white">
+                        {`-${props.discount}%`}
+                      </p>
+                    </div>
+                  </>
+                )}
               </div>
-            )}
+              {location.pathname === "/admin" && (
+                <div className="flex justify-center gap-3">
+                  <button onClick={props.editClick}>Editar</button>
+                  <button onClick={props.deleteClick}>Eliminar</button>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </li>
--- src/pages/Pharmacy.jsx
+++ src/pages/Pharmacy.jsx
@@ -9,7 +9,7 @@ const Pharmacy = () => {
       <div className="container px-6 py-10 mx-auto">
         <ul className="grid grid-cols-1 gap-8 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
           {products.map((e) => (
-            <ProductCard key={e.id} name={e.name} price={e.price} url={e.url} />
+            <ProductCard key={e.id} name={e.name} price={e.price} url={e.url} discount={e.discount} />
           ))}
         </ul>
       </div>
